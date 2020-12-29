3424294e01da47c94e86611fa101cb9e829594aa
lubio-305 <lubiopolice@gmail.com>
2020-12-29 17:14:02 +0800

    renew

--- password.py
+++ password.py
@@ -1,28 +1,12 @@
+password = 'a123456'
+i = 3 # 剩餘機會
 while True:
-	password = input('請輸入密碼: ')
-	if password == 'a123456':
-		break
-		print('登入成功')
-	elif password != 'a123456':
-		print('最多輸入3次密碼')
-		if password == 'a123456':
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功!')
+		break #逃出迴圈
+	else:
+		i = i - 1
+		print('密碼錯誤! 還有',i , '次機會')
+		if i == 0:
 			break
-			print('登入成功')
-		elif password != 'a123456':
-			print('密碼錯誤!還有2次機會')
-			if password == 'a123456':
-				break
-				print('登入成功')
-			elif password != 'a123456':
-				print('密碼錯誤!還有1次機會')
-				if password == 'a123456':
-					break
-					print('登入成功')
-				elif password != 'a123456':
-					break
-					print('密碼錯誤!帳號已鎖定')
-
-
-			
-
-
